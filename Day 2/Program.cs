a6b732c62f924cec67782cef95036295662aaefe
lekonai <lekonai@users.noreply.github.com>
2023-12-02 22:10:27 +0000

    daty 2

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -36,43 +36,39 @@ namespace Day_2
             //}
             // not gonna use this method anymore :)
             // thanks for this one wik
-            List<string> allTheGames = File.ReadAllLines("C:/Users/Omari/Documents/funny coding/c#/ADVENT OF CODE/AOC/Day 2/bin/Debug/input.txt").ToList(); // turning all those lines into one list.
+            //List<string> allTheGames = File.ReadAllLines(@"C:\Users\Omari\Documents\funny coding\c#\ADVENT OF CODE\AOC\Day 2\bin\Debug\input.txt").ToList(); // turning all those lines into one list.
 
-            foreach (var game in allTheGames)
-            {
-                int semiCI = game.IndexOf(":");
-                string ID = game.Substring(5, semiCI- 5);
-                string gameInfo = game.Substring(semiCI + 2);
-                Console.WriteLine("ID: {0}",ID);
-                Console.WriteLine("Game info: {0}", gameInfo);
-                Dictionary<string, int> counting = new Dictionary<string, int>
-                {
-                    {"red", 0 },
-                    {"blue", 0 },
-                    {"green", 0 }
-                };
-                List<string> cubeSubsets = gameInfo.Split("; ").ToList();
-                foreach (var cubeSubset in cubeSubsets)
-                {
-                    // Get the cube sets and loop over them, "12 red" is a set
-                    List<string> cubeSets = cubeSubset.Split(", ").ToList();
-                    foreach (var set in cubeSets)
-                    {
-                        // Retrieve the colour of the cubes, and the amount of cubes in the set
-                        string colour = set.Split(" ")[1];
-                        int amountOfCubesInSet = int.Parse(set.Split(" ")[0]);
-
-                        // if the colour is valid, set the amount of cubes in the set
-                        if (counting.ContainsKey(colour) && amountOfCubesInSet > counting[colour])
-                        {
-                            counting[colour] = amountOfCubesInSet;
-                        }
-                    }
-                }
-                if (counting["red"] <= 12 && counting["green"] <= 13 && counting["blue"] <= 14)
-                {
-                    sum += int.Parse(ID);
-                }
+            //foreach (var game in allTheGames)
+            //{
+            //    int cl = game.IndexOf(":");
+            //    string ID = game.Substring(5, cl);
+            //    string gameInfo = game.Substring(cl + 2);
+            //    //Console.WriteLine("ID: {0}",ID);
+            //    Console.WriteLine("info: {0}", gameInfo);
+            //    Dictionary<string, int> counting = new Dictionary<string, int>
+            //    {
+            //        {"red", 0 },
+            //        {"blue", 0 },
+            //        {"green", 0 }
+            //    };
+            //    List<string> cubeSubsets = gameInfo.Split("; ").ToList();
+            //    foreach (string cubePart in cubeSubsets)
+            //    {
+            //        List<string> cubeSets = cubePart.Split(", ").ToList(); // gets sets
+            //        foreach (string set in cubeSets)
+            //        {
+            //            string colour = set.Split(" ")[1]; // these bits get the colour + number
+            //            int amountOfCubesInSet = int.Parse(set.Split(" ")[0]);
+            //            if (counting.ContainsKey(colour) && amountOfCubesInSet > counting[colour])
+            //            {
+            //                counting[colour] = amountOfCubesInSet; // setting the number
+            //            }
+            //        }
+            //    }
+            //    if (counting["red"] <= 12 && counting["green"] <= 13 && counting["blue"] <= 14)
+            //    {
+            //        sum += int.Parse(ID);
+            //    } // doesn't work due to c# 7.3
             }
 
         }
