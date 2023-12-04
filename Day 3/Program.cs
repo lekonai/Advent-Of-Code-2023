ea28cd28376843a1e09d3e7766a9deeb9fba1a50
lekonai <lekonai@users.noreply.github.com>
2023-12-04 00:22:06 +0000

    im switching to VS2022 why isn't this working bruhhh

---
+++ Day 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<string> input = File.ReadAllLines(@"C:\Users\Omari\Documents\funny coding\c#\ADVENT OF CODE\AOC\Day 3\bin\Debug").ToList();
+            string fileN = "input.txt";
+            List<string> input = new List<string>;
+            using (StreamReader inpy = new StreamReader(fileN))
+            {
+                while (!inpy.EndOfStream)
+                {
+                    string reading = Console.ReadLine();
+                    input.Add(reading);
+
+                }
+            }
+
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    char currentChar = input[i][j];
+                    Console.WriteLine("{0}", currentChar);
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
